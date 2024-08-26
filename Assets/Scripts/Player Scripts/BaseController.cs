d7376e682e471bc3e541664d628213ffa12fc66a
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-08-26 13:25:44 +0200

    Base Controller

--- Assets/Scripts/Player Scripts/BaseController.cs
+++ Assets/Scripts/Player Scripts/BaseController.cs
@@ -13,12 +13,16 @@ public class BaseController : MonoBehaviour
     protected float max_angle = 10f;
 
     public AudioClip engine_on_sound, engine_off_sound;
+    public AudioSource soundManager;
 
     private bool is_slow;
+
+
 
     void Awake()
     {
         speed = new Vector3(0f, 0f, z_speed);
+        soundManager = GetComponent<AudioSource>();
     }
     void Update()
     {
@@ -38,5 +42,40 @@ public class BaseController : MonoBehaviour
     protected void MoveStraight()
     {
         speed = new Vector3(0f, 0f, speed.z);
+    }
+
+    protected void MoveNormal()
+    {
+        if (is_slow)
+        {
+            is_slow = false;
+
+            soundManager.Stop();
+            soundManager.clip = engine_on_sound;
+            soundManager.volume = 0.3f;
+            soundManager.Play();
+        }
+
+        speed = new Vector3(speed.x, 0, speed.z);
+    }
+
+    protected void MoveSlow()
+    {
+        if (!is_slow)
+        {
+            is_slow = true;
+
+            soundManager.Stop();
+            soundManager.clip = engine_off_sound;
+            soundManager.volume = 0.3f;
+            soundManager.Play();
+        }
+
+        speed = new Vector3(speed.x, 0, deccelerated);
+    }
+
+    protected void MoveFast()
+    {
+        speed = new Vector3(speed.x, 0, accelerated);
     }
 }
