8313a9532a3d1b49028a5dc884e0235ff0d062f4
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-08-26 13:17:27 +0200

    Scene Setup

---
+++ Assets/Scripts/Player Scripts/BaseController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    public Vector3 speed;
+    public float x_speed = 8f, z_speed = 15f;
+    public float accelerated = 15f, deccelerated = 10f;
+    public float low_sound_pitch, normal_sound_pitch, high_sound_pitch;
+
+    protected float rotationSpeed = 10f;
+    protected float max_angle = 10f;
+
+    public AudioClip engine_on_sound, engine_off_sound;
+
+    private bool is_slow;
+
+    void Awake()
+    {
+        speed = new Vector3(0f, 0f, z_speed);
+    }
+    void Update()
+    {
+        
+    }
+
+    protected void MoveLeft()
+    {
+        speed = new Vector3(-x_speed / 2f, 0f, speed.z);
+    }
+
+    protected void MoveRight()
+    {
+        speed = new Vector3(x_speed / 2f, 0f, speed.z);
+    }
+
+    protected void MoveStraight()
+    {
+        speed = new Vector3(0f, 0f, speed.z);
+    }
+}
